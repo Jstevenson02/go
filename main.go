c9c002a9ec708f2a2d00e29e899c4cf19dcda174
jstevenson02 <jacobryanstevenson@gmail.com>
2024-05-02 10:56:00 -0400

    arrays,slices,maps,loops

--- main.go
+++ main.go
@@ -1,26 +1,49 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var printValue string = "hello world!"
-	printMe(printValue)
 
-	var numerator int = 11
-	var denominator int = 2
-	var result, remainder, err = intDivision(numerator, denominator)
+	// Arrays, Slices, Maps, Loops
 
-	switch {
-	case err != nil:
-		fmt.Println(err.Error())
-	case remainder == 0:
-		fmt.Printf("The result is %v", result)
-	default:
-		fmt.Printf("The result is %v with remainder %v", result, remainder)
+	intArr := [...]int32{1, 2, 3}
+	fmt.Println(intArr)
+
+	var myMap map[string]uint8 = make(map[string]uint8)
+	fmt.Printf("My Map:%v", myMap)
+
+	var myMap2 = map[string]uint8{"Adam": 23, "Sarah": 26}
+	fmt.Println(myMap2["Adam"])
+	var age, ok = myMap2["John"]
+
+	if ok {
+		fmt.Printf("The age is %v", age)
+
+	} else {
+		fmt.Println("Invalid Name")
 	}
+
+	for name, age := range myMap2 {
+		fmt.Printf("Name: %v, Age:%v \n", name, age)
+	}
+
+	// Variables and Basic Types
+
+	// var printValue string = "hello world!"
+	// printMe(printValue)
+
+	// var numerator int = 11
+	// var denominator int = 2
+	// var result, remainder, err = intDivision(numerator, denominator)
+
+	// switch {
+	// case err != nil:
+	// 	fmt.Println(err.Error())
+	// case remainder == 0:
+	// 	fmt.Printf("The result is %v", result)
+	// default:
+	// 	fmt.Printf("The result is %v with remainder %v", result, remainder)
+	// }
 
 	// if err != nil {
 	// 	fmt.Println(err.Error())
@@ -33,18 +56,18 @@ func main() {
 	// }
 }
 
-func printMe(printValue string) {
-	fmt.Println(printValue)
-}
+// func printMe(printValue string) {
+// 	fmt.Println(printValue)
+// }
 
-func intDivision(numerator int, denominator int) (int, int, error) {
-	var err error
-	if denominator == 0 {
-		err = errors.New("cannot divide by zero")
-		return 0, 0, err
-	}
+// func intDivision(numerator int, denominator int) (int, int, error) {
+// 	var err error
+// 	if denominator == 0 {
+// 		err = errors.New("cannot divide by zero")
+// 		return 0, 0, err
+// 	}
 
-	var result int = numerator / denominator
-	var remainder int = numerator % denominator
-	return result, remainder, err
-}
+// 	var result int = numerator / denominator
+// 	var remainder int = numerator % denominator
+// 	return result, remainder, err
+// }
